711a10182beeaa030776da62b08bb8d41220b2e6
Kevin Jusino <kevinlouis759@gmail.com>
2023-05-19 02:02:52 -0400

    Adding Courses page with first table

--- script.js
+++ script.js
@@ -1,20 +1,67 @@
-fetch("semesters.json")
-.then(function(response){
-    return response.json();
-})
-.then(function(semesters){
-    let placeholder = document.querySelector("#data-output");
-    let out = "";
-    for(let semester of semesters){
-        out += `
-            <tr>
-                <td>${semester.name}</td>
-                <td>${semester.prefix}</td>
-                <td>${semester.languages}</td>
-                <td>${semester.grade}</td>
-            </tr>
-        `;
-    }
+let cor = "";
+function updateSemester(newSemester) { 
+    cor = newSemester
 
-    placeholder.innerHTML = out;
-});
+    fetch(cor)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(semesters){
+        let placeholder = document.querySelector("#data-output");
+        let out = [];
+        let newOut = [];
+        let years = 1
+        for(let semester of semesters){
+            out += `
+                <tr>
+                    <td>${semester.name}</td>
+                    <td>${semester.prefix}</td>
+                    <td>${semester.languages}</td>
+                    <td>${semester.grade}</td>
+                </tr>
+            `;
+        }
+
+        placeholder.innerHTML = out;
+    });
+}
+
+function toggleTable() {
+    var table = document.getElementById("PSU-1");
+    
+    if (table.style.display === "none") {
+      table.style.display = "table";
+    } else {
+      table.style.display = "none";
+    }
+}
+
+function toggleButtonIn() {
+    var button = document.getElementById("initial_button1");
+    
+    if (button.style.display === "none") {
+        button.style.display = "button";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+function toggleButtonH1() {
+    var button = document.getElementById("hide_button1");
+    
+    if (button.style.display === "none") {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+function toggleButtonS1() {
+    var button = document.getElementById("show_button1");
+    
+    if (button.style.display === "none") {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
+}
